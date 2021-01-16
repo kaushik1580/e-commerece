bcc153839b8330baea3eb215f1821d5a259791cd
Kaushik Kumar <kaushikkumar@Kaushiks-MacBook-Pro.local>
2021-01-16 10:17:35 +0530

    api hit

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -6,18 +6,18 @@
                     <h2 class="display-3 font-weight-bold">Login</h2>
                     <p class="font-weight-bold">Welcome back</p>
                     <br>
-                    <form action="">
+                    <form action="" @submit="postData" method="post">
                         <div class="form-group">
                             <label class="input-label">Email</label>
-                            <input type="email" class="form-control" placeholder="Email">
+                            <input type="email" class="form-control" placeholder="Email" name="author" v-model="posts.author">
                         </div>
                         <div class="form-group">
                             <label class="input-label">Password</label>
-                            <input type="password" class="form-control" placeholder="Password">
+                            <input type="password" class="form-control" placeholder="Password" name="title" v-model="posts.title">
                         </div>
                         <br>
                         <div class="form-group d-flex justify-content-center">
-                            <button class="btn btn-primary w-25" id="login">Login</button>
+                            <button class="btn btn-primary w-25" id="login" type="submit">Login</button>
                         </div>
                     </form>
                     <div class="form-group d-flex justify-content-center">
@@ -31,8 +31,29 @@
 </template>
 
 <script>
-
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios)
 export default {
+  name: 'Login',
+  data () {
+    return {
+      posts: {
+        title: null,
+        author: null
+      }
+    }
+  },
+  methods: {
+    postData (e) {
+      this.axios.get('http://10.177.1.83:9090/users/save', this.posts)
+        .this((result) => {
+          console.warn(this.posts)
+        })
+      e.preventDefault()
+    }
+  }
 }
 </script>
 
--- src/views/UserRegistration.vue
+++ src/views/UserRegistration.vue
@@ -5,31 +5,30 @@
                 <div class="login-panel bg-white text-left">
                     <h1 class="display-3 font-weight-bold">New User Registration</h1>
                     <br>
-                    <form action="">
+                    <form action="" @submit="postData" method="post">
                       <div class="form-group">
                             <label class="input-label">User Name</label>
-                            <input type="email" class="form-control" placeholder="UserName">
+                            <input type="text" class="form-control" placeholder="UserName" name="userName" v-model="posts.userName">
                         </div>
                         <div class="form-group">
                             <label class="input-label">Email</label>
-                            <input type="email" class="form-control" placeholder="Email">
+                            <input type="email" class="form-control" placeholder="Email" name="email" v-model="posts.email">
                         </div>
                         <div class="form-group">
                             <label class="input-label">Password</label>
-                            <input type="email" class="form-control" placeholder="Password">
+                            <input type="password" class="form-control" placeholder="Password" name="password" v-model="posts.password">
                         </div>
                         <div class="form-group">
-                            <label class= "input-label" for="gender">Gender</label>
-                            <br>
-                            <select id="gender" name="gender">
-                            <option value="Male">Male</option>
-                            <option value="Female">Female</option>
-                            <option value="Do not Specify">Do not Specify</option>
-                            </select>
+                            <label class="input-label">Phone Number</label>
+                            <input type="long" class="form-control" placeholder="Phone Number" name="mobileNumber" v-model="posts.mobileNumber">
+                        </div>
+                        <div class="form-group">
+                            <label class="input-label">Gender</label>
+                            <input type="text" class="form-control" placeholder="Male or Female" name="gender" v-model="posts.gender">
                         </div>
                         <br>
                         <div class="form-group d-flex justify-content-center">
-                            <button class="btn btn-primary w-25" id="login"><a v-bind:href="vuelink"></a>Register</button>
+                            <button class="btn btn-primary w-25" id="login" @click="postData"><a v-bind:href="vuelink"></a>Register</button>
                         </div>
                     </form>
                 </div>
@@ -39,11 +38,31 @@
 </template>
 
 <script>
-
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios)
 export default {
+  name: 'UserRegistration',
   data () {
     return {
-      vuelink: '../login'
+      vuelink: '../login',
+      posts: {
+        userName: '',
+        email: '',
+        password: '',
+        mobileNumber: 0,
+        gender: ''
+      }
+    }
+  },
+  methods: {
+    postData (e) {
+      this.axios.post('http://10.177.1.51:8083/users/save', this.posts)
+        .this((result) => {
+          console.warn(this.posts)
+        })
+      e.preventDefault()
     }
   }
 }
